d8f69129bcb6d1c450141ccaf30b35e35578b6a5
Marshall Demars <marshall.demars@mths.ca>
2022-11-09 02:05:53 +0000

    changes

--- greater_smaller.py
+++ greater_smaller.py
@@ -18,25 +18,24 @@ def main():
         integer_two_as_int = int(integer_two_as_string)
         if integer_one_as_int or integer_two_as_int < 0:
             print("\nPlease input only positive numbers.")
+        elif integer_one_as_int > integer_two_as_int:
+            print(
+                "\n{0} is a larger number than {1}.".format(
+                    integer_one_as_int, integer_two_as_int
+                )
+            )
+        elif integer_one_as_int < integer_two_as_int:
+            print(
+                "\n{0} is a larger number than {1}.".format(
+                    integer_two_as_int, integer_one_as_int
+                )
+            )
         else:
-            if integer_one_as_int > integer_two_as_int:
-                print(
-                    "\n{0} is a larger number than {1}.".format(
-                        integer_one_as_int, integer_two_as_int
-                    )
+            print(
+                "\n{0} is the same number as {1}.".format(
+                    integer_one_as_int, integer_two_as_int
                 )
-            elif integer_one_as_int < integer_two_as_int:
-                print(
-                    "\n{0} is a larger number than {1}.".format(
-                        integer_two_as_int, integer_one_as_int
-                    )
-                )
-            else:
-                print(
-                    "\n{0} is the same number as {1}.".format(
-                        integer_one_as_int, integer_two_as_int
-                    )
-                )
+            )
 
     except Exception:
         print("\nInvalid input.")
